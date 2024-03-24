b0fba3dbe64df0b5233ffd41d577ede20a940e5e
parwarr <parwar.habasch@gmx.de>
2024-03-24 22:28:08 +0100

    update styling

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,81 +3,82 @@
     <header class="text-center">
       <h1 class="text-4xl lg:text-6xl font-bold mb-12">Contact</h1>
     </header>
-    <div class="container mx-auto flex flex-col lg:flex-row items-center justify-center gap-8">
-      <el-form
-        ref="formRef"
-        style="max-width: 600px"
-        :model="dynamicValidateForm"
-        label-width="auto"
-        class="demo-dynamic"
-      >
-        <el-form-item
-          prop="name"
-          label="Name"
-          :rules="[
-            {
-              required: true,
-              message: 'Please input your name',
-              trigger: 'blur',
-            },
-            {
-              type: 'string',
-              message: 'The name must be a string',
-              trigger: ['blur', 'change'],
-            },
-          ]"
-        >
-          <el-input v-model="dynamicValidateForm.name" />
-        </el-form-item>
-        <el-form-item
-          prop="email"
-          label="Email"
-          :rules="[
-            {
-              required: true,
-              message: 'Please input email address',
-              trigger: 'blur',
-            },
-            {
-              type: 'email',
-              message: 'Please input correct email address',
-              trigger: ['blur', 'change'],
-            },
-          ]"
-        >
-          <el-input v-model="dynamicValidateForm.email" />
-        </el-form-item>
-        <el-form-item
-          prop="message"
-          label="Message"
-          :rules="[
-            {
-              required: true,
-              message: 'Please input your message',
-              trigger: 'blur',
-            },
-            {
-              type: 'string',
-              message: 'The message must be a string',
-              trigger: ['blur', 'change'],
-            },
-          ]"
-        >
-          <el-input v-model="dynamicValidateForm.message" />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="submitForm(formRef)">Submit</el-button>
-          <el-button @click="resetForm(formRef)">Reset</el-button>
-        </el-form-item>
-      </el-form>
-      <div class="w-full lg:w-auto flex justify-center lg:justify-end">
-        <img
-          src="https://em-content.zobj.net/source/microsoft-teams/363/rocket_1f680.png"
-          alt="rocket"
-          class="lg:h-auto lg:max-w-xs mr-16"
-          draggable="false"
-        />
-      </div>
+    <div class="container mx-auto flex flex-col lg:flex-row items-center justify-center">
+      <el-card class="bg-transparent border border-slate-600 shadow-lg p-5 flex justify-center">
+        <div class="flex-grow p-5">
+          <el-form
+            ref="formRef"
+            style="max-width: 600px"
+            :model="dynamicValidateForm"
+            label-width="auto"
+            class="demo-dynamic"
+          >
+            <el-form-item
+              prop="name"
+              :rules="[
+                {
+                  required: true,
+                  message: 'Please input your name',
+                  trigger: 'blur',
+                },
+                {
+                  type: 'string',
+                  message: 'The name must be a string',
+                  trigger: ['blur', 'change'],
+                },
+              ]"
+            >
+              <el-input v-model="dynamicValidateForm.name" placeholder="Name*" />
+            </el-form-item>
+            <el-form-item
+              prop="email"
+              :rules="[
+                {
+                  required: true,
+                  message: 'Please input email address',
+                  trigger: 'blur',
+                },
+                {
+                  type: 'email',
+                  message: 'Please input correct email address',
+                  trigger: ['blur', 'change'],
+                },
+              ]"
+            >
+              <el-input v-model="dynamicValidateForm.email" placeholder="Email*" />
+            </el-form-item>
+            <el-form-item
+              prop="message"
+              :rules="[
+                {
+                  required: true,
+                  message: 'Please input your message',
+                  trigger: 'blur',
+                },
+                {
+                  type: 'string',
+                  message: 'The message must be a string',
+                  trigger: ['blur', 'change'],
+                },
+              ]"
+            >
+              <el-input v-model="dynamicValidateForm.message" placeholder="Message*" type="textarea" />
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="submitForm(formRef)">Submit</el-button>
+              <el-button @click="resetForm(formRef)">Reset</el-button>
+            </el-form-item>
+          </el-form>
+          <div class="flex-shrink-0 lg:w-1/3">
+            <img
+              src="https://em-content.zobj.net/source/microsoft-teams/363/rocket_1f680.png"
+              alt="rocket"
+              class="lg:h-auto lg:max-w-xs"
+              draggable="false"
+            />
+          </div>
+        </div>
+      </el-card>
     </div>
   </div>
 </template>
