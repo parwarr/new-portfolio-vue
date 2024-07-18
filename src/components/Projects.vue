5fcd58d961b3acc3b3a895721edee044cdb250e4
Habasch Parwar, GHR-POD-NEX-18 <Parwar.Habasch@swisscom.com>
2024-07-19 00:18:01 +0200

    fixed vuemotion bug finallyyy

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="projects" class="p-8">
-    <div class="flex flex-col items-center justify-center">
+    <div
+      class="flex flex-col items-center justify-center"
+      v-motion-slide-visible-left
+    >
       <header class="text-center mb-12">
         <h3
           class="font-poppins font-normal text-[#05CBEE] sm:text-[20px] text-[16px] tracking-[0.2rem] mb-[8px] text-center"
@@ -21,7 +24,7 @@
 </template>
 
 <script>
-import Repository from "./Repository.vue";
+import Repository from './Repository.vue';
 export default {
   name: 'Projects',
   components: { Repository },
